731f86d851e78654fee98d363250b60d54859a2a
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-01 16:23:36 -0600

    Implements basic user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  
+  describe 'Validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should have_secure_password }
+  end
 end
